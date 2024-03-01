782b915f81a903a8401c49ba00e0783594733449
Muhammad Imran Ashraf <160359799+mimran29031@users.noreply.github.com>
2024-03-01 22:04:22 +0500

    Add files via upload

---
+++ 2 Personal Message/Personal_Message.ts
@@ -0,0 +1,8 @@
+let personName :string = '';
+personName = prompt('Enter Your Name') || "";
+if(personName !== null && personName !== ""){
+    alert(`"Hello, ${personName}  would you like to learn some Python today?"`);
+}
+else{
+    alert(" Fill Your Name");
+}
