f7ad2335f850ecad732203a77d3ba506a1c4f4d1
Muhammad Imran Ashraf <160359799+mimran29031@users.noreply.github.com>
2024-03-01 23:29:27 +0500

    Add files via upload

---
+++ 7 - 8 Number Eight/Number_Eight.ts
@@ -0,0 +1,12 @@
+// 7 : Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+// 8 : You should create four lines that look like this:
+// console.log(5 + 3)
+
+
+console.log(6+2);  //addition
+
+console.log(10-2); //subtraction
+
+console.log(4*2);  //multiplication
+
+console.log(16/2); //divid
