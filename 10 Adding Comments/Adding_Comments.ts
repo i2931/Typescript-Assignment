255dbcd969b270825bca8cd9e342785a9a0b3321
Muhammad Imran Ashraf <160359799+mimran29031@users.noreply.github.com>
2024-03-02 23:17:48 +0500

    Add files via upload

---
+++ 10 Adding Comments/Adding_Comments.ts
@@ -0,0 +1,9 @@
+//Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+
+
+//This program Written By : M Imran Ashaf
+//Date : 23/2/2024
+//This Program will Written Hello World
+
+console.log("Hello World");
