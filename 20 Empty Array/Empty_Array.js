3032b21194ebb0341954ae84b408461c83d51a0a
Muhammad Imran Ashraf <160359799+mimran29031@users.noreply.github.com>
2024-03-03 16:33:41 +0500

    Add files via upload

---
+++ 20 Empty Array/Empty_Array.js
@@ -0,0 +1,9 @@
+"use strict";
+//Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+let items = ["Pakistan", "Karachi", "Urdu"];
+// write a program that creates a list containing these item.
+let item2 = [];
+item2.push("Pakistan");
+item2.push("Karachi");
+item2.push("Urdu");
+console.log(item2);
