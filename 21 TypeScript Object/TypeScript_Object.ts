da31d2a6dc4382b9cffb85a07ac27112fe04f067
Muhammad Imran Ashraf <160359799+mimran29031@users.noreply.github.com>
2024-03-03 16:38:40 +0500

    Add files via upload

---
+++ 21 TypeScript Object/TypeScript_Object.ts
@@ -0,0 +1,20 @@
+//Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+// Write a program that creates Objects containing these items.
+
+interface person{
+    age : number,
+    name : string,
+    nationality : string,
+    Student : Boolean,
+}
+
+let person :person = {
+    
+    age : 30,
+    name : "Sufyan",
+    nationality :"pakistani",
+    Student : true
+}
+console.log(person);
+
