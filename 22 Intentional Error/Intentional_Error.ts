6aceae39f39063d41e69c4ce8aa92c5b97c8fe1f
Muhammad Imran Ashraf <160359799+mimran29031@users.noreply.github.com>
2024-03-03 16:50:32 +0500

    Add files via upload

---
+++ 22 Intentional Error/Intentional_Error.ts
@@ -0,0 +1,12 @@
+// Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+
+// Creating array
+let newArray :string[] = ['will smith', 'james Bond', 'Tom Cruise' ];
+
+// Accessing array by inbox
+console.log (newArray[5]);
+
+// Make Sure you correct the error before closing the PerformanceNavigationTiming.
+
+// Correct the error
+console.log(newArray);
