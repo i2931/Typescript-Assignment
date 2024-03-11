7ecb4754e04d9024f54ec87d399c03a526f1899b
Muhammad Imran Ashraf <160359799+i2931@users.noreply.github.com>
2024-03-11 20:54:02 +0500

    Delete 14 Guest List directory

--- 14 Guest List/Guest_List.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-//Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
-let guest_list = ["Fawad", "Zahid", "Iqbal", "Mustafa"];
-for (let i = 0; i < guest_list.length; i++) {
-    console.log("Dr.Mr. " + guest_list[i] + ",\n\n It is our Pleasure to invite you in our party.\n\n Thank You \n\n");
-}
