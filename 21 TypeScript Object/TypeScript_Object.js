dddd17a2cf78b7ebebe51468d6de4a3da678ba9e
Muhammad Imran Ashraf <160359799+i2931@users.noreply.github.com>
2024-03-11 20:55:41 +0500

    Delete 21 TypeScript Object directory

--- 21 TypeScript Object/TypeScript_Object.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-//Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
-let person = {
-    age: 30,
-    name: "Sufyan",
-    nationality: "pakistani",
-    Student: true
-};
-console.log(person);
