0b639cd12b0809765ef16c680f2d2b880f1cf39c
Muhammad Imran Ashraf <160359799+i2931@users.noreply.github.com>
2024-03-11 20:55:52 +0500

    Delete 22 Intentional Error directory

--- 22 Intentional Error/Intentional_Error.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
-// Creating array
-let newArray = ['will smith', 'james Bond', 'Tom Cruise'];
-// Accessing array by inbox
-console.log(newArray[5]);
-// Make Sure you correct the error before closing the PerformanceNavigationTiming.
-// Correct the error
-console.log(newArray);
