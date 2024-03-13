6c598a4a1952d4573d8024d8059d4f265685eb93
Muhammad Imran Ashraf <160359799+i2931@users.noreply.github.com>
2024-03-13 23:02:21 +0500

    Add files via upload

---
+++ 17 Shrinking Guest List/Guest_List.ts
@@ -0,0 +1,48 @@
+//Written by: M Imran Ashraf
+// Roll No. : 00379541
+
+
+let guest_list :string[] = ["Fawad", "Zahid", "Iqbal", "Mustafa"];
+
+// for(let i=0; i < guest_list.length; i++){
+
+//     console.log("Dr.Mr. " + guest_list[i] +",\n\n It is our Pleasure to invite you in our party.\n\n Thank You \n\n" )
+// }
+let change_guest :string ="Iqbal";
+let new_guest :string ="Mubashir";
+guest_list[2] = new_guest ;
+
+// for(let i=0; i < guest_list.length; i++){
+
+//     console.log("Dr.Mr. " + guest_list[i] +",\n\n It is our Pleasure to invite you in our party.\n\n Thank You \n\n" )
+// }
+console.log(`Mr , ${change_guest} is Not Coming `);
+console.log ("Good News! We find Big Table So we are inviting 3 more Guests.");
+
+// Add 3 people in array.
+guest_list.unshift("Kamran");
+guest_list.splice(2,0, "Farhan");
+guest_list.push("Naveed");
+
+// 6 guest array print.
+for(let i=0; i < guest_list.length; i++){
+
+    console.log("Dr.Mr. " + guest_list[i] +",\n\n It is our Pleasure to invite you in our party.\n\n Thank You \n\n" )
+}
+//Sorry Message to people not inviting.
+console.log ("\n Sorry we can not arrange big table, Only Two people will be invited.");
+
+// This Code remove guest.
+while(guest_list.length > 2){
+let remove_guest = guest_list.pop()
+console.log(`Sorry ${remove_guest}, Your Not Invited to Dinner`);
+}
+
+// 2 Pepole Invit Guest.
+for(let i=0; i < guest_list.length; i++){
+    console.log("Dr.Mr. " + guest_list[i] +",\n\n You are still invited.\n\n Thank You \n\n" );
+}
+
+// 4 Guest romove.
+guest_list.splice(0,2);
+console.log(guest_list);
