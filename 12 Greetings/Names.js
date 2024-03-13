4a8a60f9e54326f7dcaa7dbe98cd7140bf4e1462
Muhammad Imran Ashraf <160359799+i2931@users.noreply.github.com>
2024-03-13 22:53:03 +0500

    Add files via upload

---
+++ 12 Greetings/Names.js
@@ -0,0 +1,8 @@
+"use strict";
+//Written by: M Imran Ashraf
+// Roll No. : 00379541
+let names = ["Fawad", "Zahid", "Iqbal", "Mustafa"];
+const message = "Welcome To Governor House ";
+for (let i = 0; i < names.length; i++) {
+    console.log(message + names[i]);
+}
---
+++ 12 Greetings/Names.ts
@@ -0,0 +1,11 @@
+//Written by: M Imran Ashraf
+// Roll No. : 00379541
+
+
+let names :string[] = ["Fawad", "Zahid", "Iqbal", "Mustafa"];
+const message :string ="Welcome To Governor House ";
+
+for(let i=0 ; i<names.length; i++){
+
+    console.log(message + names[i])
+}
