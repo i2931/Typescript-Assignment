e0279648e29c2106ff76b1d310f17734d1aa5c3c
Muhammad Imran Ashraf <160359799+i2931@users.noreply.github.com>
2024-03-13 22:50:33 +0500

    Add files via upload

---
+++ 10 Adding Comments/Adding_Comments.js
@@ -0,0 +1,6 @@
+"use strict";
+//This program Written By : M Imran Ashaf
+//Date : 23/2/2024
+//This Program will Written Hello World
+// Roll No. : 00379541
+console.log("Hello World");
---
+++ 10 Adding Comments/Adding_Comments.ts
@@ -0,0 +1,8 @@
+
+//This program Written By : M Imran Ashaf
+//Date : 23/2/2024
+//This Program will Written Hello World
+// Roll No. : 00379541
+
+
+console.log("Hello World");
