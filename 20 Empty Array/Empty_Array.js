3a8cededc3f22a3eec8e4dcb4a2a4bab962e9c70
Muhammad Imran Ashraf <160359799+i2931@users.noreply.github.com>
2024-03-13 23:06:23 +0500

    Add files via upload

---
+++ 20 Empty Array/Empty_Array.js
@@ -0,0 +1,10 @@
+"use strict";
+//Written by: M Imran Ashraf
+// Roll No. : 00379541
+let items = ["Pakistan", "Karachi", "Urdu"];
+// write a program that creates a list containing these item.
+let item2 = [];
+item2.push("Pakistan");
+item2.push("Karachi");
+item2.push("Urdu");
+console.log(item2);
---
+++ 20 Empty Array/Empty_Array.ts
@@ -0,0 +1,15 @@
+//Written by: M Imran Ashraf
+// Roll No. : 00379541
+
+
+let items :string[] =["Pakistan", "Karachi","Urdu"];
+
+
+// write a program that creates a list containing these item.
+
+let item2 :string[] = [];
+item2.push("Pakistan");
+item2.push("Karachi");
+item2.push("Urdu");
+
+console.log(item2);
