acbc4f3755369db48d6fdb94203e90b634077b1a
Muhammad Imran Ashraf <160359799+i2931@users.noreply.github.com>
2024-03-13 23:09:19 +0500

    Add files via upload

---
+++ 22 Intentional Error/Intentional_Error.js
@@ -0,0 +1,10 @@
+"use strict";
+//Written by: M Imran Ashraf
+// Roll No. : 00379541
+// Creating array
+let newArray = ['will smith', 'james Bond', 'Tom Cruise'];
+// Accessing array by inbox
+console.log(newArray[5]);
+// Make Sure you correct the error before closing the PerformanceNavigationTiming.
+// Correct the error
+console.log(newArray);
---
+++ 22 Intentional Error/Intentional_Error.ts
@@ -0,0 +1,14 @@
+//Written by: M Imran Ashraf
+// Roll No. : 00379541
+
+
+// Creating array
+let newArray :string[] = ['will smith', 'james Bond', 'Tom Cruise' ];
+
+// Accessing array by inbox
+console.log (newArray[5]);
+
+// Make Sure you correct the error before closing the PerformanceNavigationTiming.
+
+// Correct the error
+console.log(newArray);
