53e5ce20f87db65c4702d5772d905e9e74a43a45
Muhammad Imran Ashraf <160359799+i2931@users.noreply.github.com>
2024-03-13 22:30:48 +0500

    Delete 4 Famous Quote directory

--- 4 Famous Quote/Famous_Quote.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-//Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
-console.log('Albert Einstein once said, “Imagination is more important than knowledge.');
--- 4 Famous Quote/Famous_Quote.ts
+++
@@ -1,3 +0,0 @@
-//Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
-
-console.log('Albert Einstein once said, “Imagination is more important than knowledge.');
