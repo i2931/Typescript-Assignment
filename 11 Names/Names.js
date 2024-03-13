d90c9505bde26fcecaa57db85a1ff0baa3ba6003
Muhammad Imran Ashraf <160359799+i2931@users.noreply.github.com>
2024-03-13 22:51:57 +0500

    Add files via upload

---
+++ 11 Names/Names.js
@@ -0,0 +1,7 @@
+"use strict";
+//Written by: M Imran Ashraf
+// Roll No. : 00379541
+let names = ["Fawad", "Zahid", "Iqbal", "Mustafa"];
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ 11 Names/Names.ts
@@ -0,0 +1,10 @@
+//Written by: M Imran Ashraf
+// Roll No. : 00379541
+
+
+let names :string[] = ["Fawad", "Zahid", "Iqbal", "Mustafa"];
+
+for(let i=0 ; i<names.length; i++){
+
+    console.log(names[i])
+}
