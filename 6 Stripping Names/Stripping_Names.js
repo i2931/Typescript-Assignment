3ff6495a87172ccfd3c280b981bc75c9f018151c
Muhammad Imran Ashraf <160359799+i2931@users.noreply.github.com>
2024-03-13 22:44:21 +0500

    Add files via upload

---
+++ 6 Stripping Names/Stripping_Names.js
@@ -0,0 +1,5 @@
+"use strict";
+//Written by: M Imran Ashraf
+// Roll No. : 00379541
+const personName = " \n\tImran Ashraf\t\n";
+console.log(personName);
---
+++ 6 Stripping Names/Stripping_Names.ts
@@ -0,0 +1,6 @@
+//Written by: M Imran Ashraf
+// Roll No. : 00379541
+
+
+const personName :string =" \n\tImran Ashraf\t\n";
+console.log(personName)
