96310495794b22086ccd75371f19c740e678051a
Muhammad Imran Ashraf <160359799+i2931@users.noreply.github.com>
2024-03-13 22:47:06 +0500

    Add files via upload

---
+++ 7 - 8 Number Eight/Number_Eight.ts
@@ -0,0 +1,11 @@
+//Written by: M Imran Ashraf
+// Roll No. : 00379541
+
+
+console.log(6+2);  //addition
+
+console.log(10-2); //subtraction
+
+console.log(4*2);  //multiplication
+
+console.log(16/2); //divid
