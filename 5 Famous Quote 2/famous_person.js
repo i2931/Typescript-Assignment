fcdfb8dec4cf1a91204714c1dec0f6dd5c1b7425
Muhammad Imran Ashraf <160359799+i2931@users.noreply.github.com>
2024-03-13 22:42:50 +0500

    Add files via upload

---
+++ 5 Famous Quote 2/famous_person.js
@@ -0,0 +1,6 @@
+"use strict";
+//Written by: M Imran Ashraf
+// Roll No. : 00379541
+let famous_person = "Albert Einstein";
+let message = "Imagination is more important than knowledge.";
+console.log(`${famous_person} once said,  ${message}`);
---
+++ 5 Famous Quote 2/famous_person.ts
@@ -0,0 +1,7 @@
+//Written by: M Imran Ashraf
+// Roll No. : 00379541
+
+
+let famous_person :string = "Albert Einstein";
+let message :string = "Imagination is more important than knowledge.";
+console.log( `${famous_person} once said,  ${message}`);
