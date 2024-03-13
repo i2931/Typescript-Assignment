3a0b9231c240a9db54942885d916a2e9f1c99c57
Muhammad Imran Ashraf <160359799+i2931@users.noreply.github.com>
2024-03-13 22:56:40 +0500

    Add files via upload

---
+++ 14 Guest List/Guest_List.js
@@ -0,0 +1,7 @@
+"use strict";
+//Written by: M Imran Ashraf
+// Roll No. : 00379541
+let guest_list = ["Fawad", "Zahid", "Iqbal", "Mustafa"];
+for (let i = 0; i < guest_list.length; i++) {
+    console.log("Dr.Mr. " + guest_list[i] + ",\n\n It is our Pleasure to invite you in our party.\n\n Thank You \n\n");
+}
---
+++ 14 Guest List/Guest_List.ts
@@ -0,0 +1,10 @@
+//Written by: M Imran Ashraf
+// Roll No. : 00379541
+
+
+let guest_list :string[] = ["Fawad", "Zahid", "Iqbal", "Mustafa"];
+
+for(let i=0; i < guest_list.length; i++){
+
+    console.log("Dr.Mr. " + guest_list[i] +",\n\n It is our Pleasure to invite you in our party.\n\n Thank You \n\n" )
+}
