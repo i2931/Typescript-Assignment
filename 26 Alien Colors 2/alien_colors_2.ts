e4604b87210730643ec7a42b67fe687705b302a2
Muhammad Imran Ashraf <160359799+i2931@users.noreply.github.com>
2024-03-13 23:16:02 +0500

    Add files via upload

---
+++ 26 Alien Colors 2/alien_colors_2.ts
@@ -0,0 +1,30 @@
+//Written by: M Imran Ashraf
+// Roll No. : 00379541
+
+
+// Alien Colors #2: Choose a color for an alien as you did in Exercise 25
+let alienColors : string = "red";
+
+//write an if-else chain.
+//if the alien color is "red"
+// print a statement that the player just earned 5 points for shooting the alien.
+
+if(alienColors === "red"){
+console.log("player just earned 5 points for shooting the alien.");
+}
+
+else{
+console.log ("player just earned 10 points")
+}
+
+// • If the alien’s color isn’t green, print a statement that the .
+
+alienColors = "green"
+if(alienColors === "red"){
+console.log("player just earned 5 points for shooting the alien.");
+}
+
+else{
+console.log ("player just earned 10 points")
+}
+// • Write one version of this program that runs the if block and another that runs the else block.
