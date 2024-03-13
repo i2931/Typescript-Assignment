b988646591e676524c514dd198094a3bb178877e
Muhammad Imran Ashraf <160359799+i2931@users.noreply.github.com>
2024-03-13 22:54:48 +0500

    Add files via upload

---
+++ 13 Your Own Array/Your_Own_Array.js
@@ -0,0 +1,7 @@
+"use strict";
+//Written by: M Imran Ashraf
+// Roll No. : 00379541
+let transportation = ["Car", "Train", "Cycel", "Motercycle"];
+for (let i = 0; i < transportation.length; i++) {
+    console.log("I would Like to own a " + transportation[i]);
+}
---
+++ 13 Your Own Array/Your_Own_Array.ts
@@ -0,0 +1,10 @@
+//Written by: M Imran Ashraf
+// Roll No. : 00379541
+
+
+let transportation :string[] = ["Car", "Train", "Cycel", "Motercycle"];
+
+for(let i=0; i< transportation.length; i++){
+
+    console.log("I would Like to own a " + transportation[i])
+}
