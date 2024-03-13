110ef8708624dbd12dbe422fd9241944a057bf3b
Muhammad Imran Ashraf <160359799+i2931@users.noreply.github.com>
2024-03-13 23:19:57 +0500

    Add files via upload

---
+++ 29 Favorite Fruit/favorite_fruit.js
@@ -0,0 +1,19 @@
+"use strict";
+//Written by: M Imran Ashraf
+// Roll No. : 00379541
+const favorite_fruits = ["mango", "apple", "strawberry"];
+if (favorite_fruits.includes("mango")) {
+    console.log("I like mango!");
+}
+if (favorite_fruits.includes("apple")) {
+    console.log("I like apples!");
+}
+if (favorite_fruits.includes("strawberry")) {
+    console.log("I like strawberries!");
+}
+if (favorite_fruits.includes("orange")) {
+    console.log("I like oranges!");
+}
+if (favorite_fruits.includes("apricot")) {
+    console.log("I like apricot!");
+}
---
+++ 29 Favorite Fruit/favorite_fruit.ts
@@ -0,0 +1,24 @@
+//Written by: M Imran Ashraf
+// Roll No. : 00379541
+
+const favorite_fruits: string[] = ["mango" ,"apple",   "strawberry"];
+
+if (favorite_fruits.includes("mango")) {
+    console.log("I like mango!");
+}
+
+if (favorite_fruits.includes("apple")) {
+    console.log("I like apples!");
+}
+
+if (favorite_fruits.includes("strawberry")) {
+    console.log("I like strawberries!");
+}
+
+if (favorite_fruits.includes("orange")) {
+    console.log("I like oranges!");
+}
+
+if (favorite_fruits.includes("apricot")) {
+    console.log("I like apricot!");
+}
