f18a5ac578e5e4aeacd6290650d4c4f99c3294a2
Muhammad Imran Ashraf <160359799+i2931@users.noreply.github.com>
2024-03-13 23:08:04 +0500

    Add files via upload

---
+++ 21 Typescript object/TypeScript_Object.js
@@ -0,0 +1,10 @@
+"use strict";
+//Written by: M Imran Ashraf
+// Roll No. : 00379541
+let person = {
+    age: 30,
+    name: "Sufyan",
+    nationality: "pakistani",
+    Student: true
+};
+console.log(person);
---
+++ 21 Typescript object/TypeScript_Object.ts
@@ -0,0 +1,22 @@
+//Written by: M Imran Ashraf
+// Roll No. : 00379541
+
+
+// Write a program that creates Objects containing these items.
+
+interface person{
+    age : number,
+    name : string,
+    nationality : string,
+    Student : Boolean,
+}
+
+let person :person = {
+    
+    age : 30,
+    name : "Sufyan",
+    nationality :"pakistani",
+    Student : true
+}
+console.log(person);
+
