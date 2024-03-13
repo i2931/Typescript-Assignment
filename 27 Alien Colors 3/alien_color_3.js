7e4902c310159a8d8aaa071edd5dbe18cdc8a615
Muhammad Imran Ashraf <160359799+i2931@users.noreply.github.com>
2024-03-13 23:17:21 +0500

    Add files via upload

---
+++ 27 Alien Colors 3/alien_color_3.js
@@ -0,0 +1,45 @@
+"use strict";
+//Written by: M Imran Ashraf
+// Roll No. : 00379541
+let alienColor = "green";
+//version 1 of the program
+if (alienColor === "green") {
+    console.log("player earned 5 points.");
+}
+else if (alienColor === "yellow") {
+    console.log("player earned 10 points.");
+}
+else if (alienColor === "red") {
+    console.log("player earned 15 points.");
+}
+else {
+    console.log(" please slect right color");
+}
+//version 2 of the program
+alienColor = "yellow";
+if (alienColor === "green") {
+    console.log("player earned 5 points.");
+}
+else if (alienColor === "yellow") {
+    console.log("player earned 10 points.");
+}
+else if (alienColor === "red") {
+    console.log("player earned 15 points.");
+}
+else {
+    console.log(" please slect right color");
+}
+//version 3 of the program
+alienColor = "red";
+if (alienColor === "green") {
+    console.log("player earned 5 points.");
+}
+else if (alienColor === "yellow") {
+    console.log("player earned 10 points.");
+}
+else if (alienColor === "red") {
+    console.log("player earned 15 points.");
+}
+else {
+    console.log(" please slect right color");
+}
