780622a0057e5568fdb5a8815b26ccc1a471703d
Muhammad Imran Ashraf <160359799+i2931@users.noreply.github.com>
2024-03-13 23:10:33 +0500

    Add files via upload

---
+++ 23 Conditional Tests/conditional_tests.js
@@ -0,0 +1,34 @@
+"use strict";
+//Written by: M Imran Ashraf
+// Roll No. : 00379541
+let car = "subaru";
+//Test No 1
+console.log("Is car === 'subaru'? I predict True.");
+console.log(car === "subaru");
+// Test No 2
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == "subaru");
+//Test No  3
+console.log("Is car != 'Honda'? I predict True.");
+console.log(car != "Honda");
+//Test No 4
+console.log("Is car !== 'Honda'? I predict True.");
+console.log(car !== "Honda");
+// Test No 5
+console.log("Is car.Upper Case == 'SUBARU'? I predict True.");
+console.log(car.toUpperCase() == "SUBARU");
+// Test No 6
+console.log("Is car.Lower Case == 'SUBARU'? I predict Fals.");
+console.log(car.toLowerCase() == "SUBARU");
+//Test No 7
+console.log("Is car == 'Honda'? I predict Fals.");
+console.log(car == "Honda");
+// Test No 8
+console.log("Is car === 'Honda'? I predict Fals.");
+console.log(car === "Honda");
+//Test No 9
+console.log("Is car == 'BUS'? I predict Fals.");
+console.log(car == "BUS");
+//Test No 10
+console.log("Is car == 'Bike'? I predict Fals.");
+console.log(car == "Bike");
