1eba6d5311abf9df74dfc9057e3ff5d2c679b652
Muhammad Imran Ashraf <160359799+i2931@users.noreply.github.com>
2024-03-13 22:48:39 +0500

    Add files via upload

---
+++ 9 Favorite Number/Favorite_Number.js
@@ -0,0 +1,6 @@
+"use strict";
+//Written by: M Imran Ashraf
+// Roll No. : 00379541
+const favorite_number = 9;
+const message = "My Favorite Number is " + favorite_number;
+console.log(message);
---
+++ 9 Favorite Number/Favorite_Number.ts
@@ -0,0 +1,7 @@
+//Written by: M Imran Ashraf
+// Roll No. : 00379541
+
+
+const favorite_number :number = 9;
+const message :string = "My Favorite Number is " + favorite_number;
+console.log(message);
