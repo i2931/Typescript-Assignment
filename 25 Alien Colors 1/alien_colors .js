21b81f9c838f48ba23a8653093b276d38abcf031
Muhammad Imran Ashraf <160359799+i2931@users.noreply.github.com>
2024-03-13 23:14:43 +0500

    Add files via upload

---
+++ 25 Alien Colors 1/alien_colors .js
@@ -0,0 +1,6 @@
+"use strict";
+// Create a variable called alien_color and assign
+let alien_color = "red";
+if (alien_color === "red") {
+    console.log("player Just earned 5 points !");
+}
---
+++ 25 Alien Colors 1/alien_colors.js
@@ -0,0 +1,13 @@
+"use strict";
+//Written by: M Imran Ashraf
+// Roll No. : 00379541
+// Create a variable called alien_color and assign
+let alien_color = "red";
+if (alien_color === "red") {
+    console.log("player Just earned 5 points !");
+}
+// Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
+alien_color = "red";
+if (alien_color === "green") {
+    console.log("player Just earned 5 points !");
+}
