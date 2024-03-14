2bf5bdc475c1fd1d378e92b9d979854b9a9f43eb
Muhammad Imran Ashraf <160359799+i2931@users.noreply.github.com>
2024-03-14 21:21:10 +0500

    Add files via upload

---
+++ 34 Pizzas/pizzas.js
@@ -0,0 +1,11 @@
+"use strict";
+//Written by: M Imran Ashraf
+// Roll No. : 00379541
+//  names of the pizzas
+let pizzas = ["Pepperoni", "Cuban", "BBQ Chicken"];
+// Loop Use kiya he.
+for (let pizza of pizzas) {
+    console.log(`I like ${pizza} pizza.`);
+}
+// Result
+console.log("I really love pizza!");
