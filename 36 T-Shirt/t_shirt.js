e90aac4f30dc9aae7f571f4be336826dcf627d59
Muhammad Imran Ashraf <160359799+i2931@users.noreply.github.com>
2024-03-14 21:23:08 +0500

    Add files via upload

---
+++ 36 T-Shirt/t_shirt.js
@@ -0,0 +1,7 @@
+"use strict";
+//Written by: M Imran Ashraf
+// Roll No. : 00379541
+function make_shirt(size, message) {
+    console.log(`Creating a ${size} shirt with the message: "${message}"`);
+}
+make_shirt("large", "Hello, world!");
---
+++ 36 T-Shirt/t_shirt.ts
@@ -0,0 +1,9 @@
+//Written by: M Imran Ashraf
+// Roll No. : 00379541
+
+
+function make_shirt(size: string, message: string): void {
+    console.log(`Creating a ${size} shirt with the message: "${message}"`);
+  }
+  
+  make_shirt("large", "Hello, world!");
