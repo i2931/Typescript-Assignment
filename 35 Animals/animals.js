b8605a6de87293649f43db223e5dd1fff2a0ee9b
Muhammad Imran Ashraf <160359799+i2931@users.noreply.github.com>
2024-03-14 21:22:07 +0500

    Add files via upload

---
+++ 35 Animals/animals.js
@@ -0,0 +1,11 @@
+"use strict";
+//Written by: M Imran Ashraf
+// Roll No. : 00379541
+// animals
+const animals = ["dog", "cat", "rabbit"];
+// statement about each animal
+for (let animal of animals) {
+    console.log(`A ${animal} would make a great pet.`);
+}
+// Result
+console.log("Any of these animals would make a great pet!");
---
+++ 35 Animals/animals.ts
@@ -0,0 +1,14 @@
+//Written by: M Imran Ashraf
+// Roll No. : 00379541
+
+
+// animals
+const animals: string[] = ["dog", "cat", "rabbit"];
+
+// statement about each animal
+for (let animal of animals) {
+  console.log(`A ${animal} would make a great pet.`);
+}
+
+// Result
+console.log("Any of these animals would make a great pet!");
