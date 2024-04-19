b5537a30a69e74285fb447c739dfb733302a3d9b
Muhammad Imran Ashraf <160359799+i2931@users.noreply.github.com>
2024-04-19 23:13:39 +0500

    Add files via upload

---
+++ 3 Name Cases/Name_Cases.js
@@ -0,0 +1,18 @@
+"use strict";
+//Written by: M Imran Ashraf
+// Roll No. : 00379541
+let personName = "";
+personName = prompt("Enter Your Name") || "";
+let lowercase = personName.toLowerCase();
+let uppercase = personName.toUpperCase();
+let titlecase = personName.split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(" ");
+if (personName !== null && personName !== "") {
+    alert(`
+    Lowerase : ${lowercase}
+    UpperCase : ${uppercase}
+    TitleCase : ${titlecase}
+    `);
+}
+else {
+    alert(" Fill Your Name ");
+}
